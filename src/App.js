431b09d7f2860748a1e2ac761328cfdcbf78e518
jaehyeon99 <janhyun99@naver.com>
2022-05-01 11:00:00 +0000

    Add Date && fixed link

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
       <h1>코로나 예방접종센터 위치정보 알리미</h1>
 
       <a className="link " href="https://ncvr.kdca.go.kr/"><button>코로나 백신 예약 바로가기 🖲️</button></a>
-      <a className="link " href="https://ncvr.kdca.go.kr/"><button>코로나 백신 안내 🖲️</button></a>
+      <a className="link " href="https://nip.kdca.go.kr/irgd/covid.do?MnLv1=1&MnLv2=2"><button>코로나 백신 주의사항 🖲️</button></a>
       <div className="LiveCovid">
         <Live />
       </div>
 
       <div className="container">
         <div className="frame">
-          <h2>💉 코로나 예방접종센터 위치정보 💉 </h2>
+          <h2> 💉 코로나 예방접종센터 위치정보 💉 </h2>
           <h4><Vaccine /></h4>
         </div>
         <div className="frame_2">
--- src/Components/Live.js
+++ src/Components/Live.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
+
 function Live() {
 
     const [Live, setLive] = useState("");
+    const today = new Date();
+    const date = today.toDateString();
     useEffect(() => {
         axios.get("https://api.covid19api.com/live/country/south-korea/status/confirmed/date/2020-01-01T13:13:30Z")
             .then(result => setLive(result))
             .catch(e => console.log(e));
+
     }, [])
 
     if (!Live) {
@@ -17,17 +21,19 @@ function Live() {
         </div>
     }
 
+
     return (
         <div>
             {
-                console.log(Live.data[39])
+
+                console.log(Live.data[Live.data.length - 1])
             }
             <div className="board">
-                <h4>금일 00 : 00 기준</h4>
+                <h4>{date}<br />00 : 00 기준</h4>
                 <div className="number">
-                    <div className="confirmed"> <span className="topic">확진자</span> <br /> {Live.data[39].Confirmed} 명 <br /></div>
-                    <div className="recovered"><span className="topic">완치자</span>   <br />  {Live.data[39].Recovered} 명 <br /></div>
-                    <div className="deathed"> <span className="topic">사망자</span> <br />  {Live.data[39].Deaths} 명 <br /> </div>
+                    <div className="confirmed"> <span className="topic">확진자</span> <br /> {Live.data[Live.data.length - 1].Confirmed} 명 <br /></div>
+                    <div className="recovered"><span className="topic">완치자</span>   <br />  {Live.data[Live.data.length - 1].Recovered} 명 <br /></div>
+                    <div className="deathed"> <span className="topic">사망자</span> <br />  {Live.data[Live.data.length - 1].Deaths} 명 <br /> </div>
                 </div>
             </div>
         </div>
